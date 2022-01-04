9f0c9140ef765505b071f38acc5ce35b02d8fb6d
Ioram Gordadze <Yoh0xFF@gmail.com>
2022-01-04 20:01:54 +0400

    Change indents.

--- hello-gradle/hello.groovy
+++ hello-gradle/hello.groovy
@@ -1,25 +1,25 @@
 def courses = ["neo4j", "maven-101", "maven-102", "Spring"]
 
 courses.each {
-    println it
+  println it
 }
 
 class Person {
-    String firstName
-    String lastName
+  String firstName
+  String lastName
 
-    Person(firstName, lastName) {
-        this.firstName = firstName
-        this.lastName = lastName
-    }
+  Person(firstName, lastName) {
+    this.firstName = firstName
+    this.lastName = lastName
+  }
 
-    def fullName() {
-        return this.firstName + " " + this.lastName
-    }
+  def fullName() {
+    return this.firstName + " " + this.lastName
+  }
 }
 
 def people = [new Person("Ada", "Lovelace"), new Person("Alan", "Turing"), new Person("Dennis", "Ritchie"), new Person("Brian", "Kernighan")]
 
 people.each {
-    println it.fullName()
+  println it.fullName()
 }
