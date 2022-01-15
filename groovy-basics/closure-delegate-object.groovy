f2a3adae39495a47f375aabd0015eb68a5e8fe28
Ioram Gordadze <Yoh0xFF@gmail.com>
2022-01-15 20:18:55 +0400

    Add groovy delegate example.

---
+++ groovy-basics/closure-delegate-object.groovy
@@ -0,0 +1,18 @@
+class Person {
+  String firstName
+  String lastName
+
+  Person(String firstName, String lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  def executeInside(Closure c) {
+    c.delegate = this // Change the closure owner
+    c()
+  }
+}
+
+def p = new Person('Dennis', 'Ritchie')
+
+p.executeInside { println "Hello $firstName $lastName" }
